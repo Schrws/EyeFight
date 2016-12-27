efeb551d59c7f86337323552721349aac502ab59
Kevin Roh <suzynjun99@naver.com>
2016-12-27 22:02:41 +0900

    GOING HOME

--- app/src/main/java/sasa/pky/eyefight/Intro.java
+++ app/src/main/java/sasa/pky/eyefight/Intro.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 /**
  * Created by SchrwsK on 2016-12-27.
@@ -15,12 +17,35 @@ public class Intro extends AppCompatActivity {
         super.onCreate(bundle);
         setContentView(R.layout.intro);
 
+        final RelativeLayout layout = (RelativeLayout) findViewById(R.id.optionlayout);
+
         Button single = (Button) findViewById(R.id.single);
+        Button random = (Button) findViewById(R.id.random);
+        Button option = (Button) findViewById(R.id.option);
+        Button x = (Button) findViewById(R.id.x);
         single.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intro.this, Main.class);
                 startActivity(intent);
+            }
+        });
+        random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Intro.this, "기능 준비중입니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+        option.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.VISIBLE);
+            }
+        });
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setVisibility(View.GONE);
             }
         });
     }
